3c081ee1419374074fca6d6081c305523aa70664
Garrett Holbrook <garrett92895@yahoo.com>
2014-08-22 15:42:39 -0600

    Zed classes complete
    Zed classes are complete but some things need to be done pertaining to
    them. Added a HasDodged property to the character class.

--- ZombieApocalypseSimulator/Models/Characters/Classess/FastAttack.cs
+++ ZombieApocalypseSimulator/Models/Characters/Classess/FastAttack.cs
@@ -8,14 +8,51 @@ namespace ZombieApocalypseSimulator.Models.Characters.Classess
 {
     public class FastAttack: Zed
     {
-//        public override int toDodgeRangedAttack()
-//        {
-//            return toDodge() - 6;
-//        }
+       public FastAttack()
+        {
 
-		public override int MeleeAttack()
-		{
-			return 0;
-		}
+            PhysicalStrength = DieRoll.RollOne(6) + 18;
+            PhysicalProwess = DieRoll.RollOne(6) + 7;
+            PhysicalEndurance = DieRoll.RollOne(6) + 15;
+            Speed = DieRoll.RollOne(7) + 16;
+			MaxSDC = rollsdc();
+			sdc = MaxSDC;
+			MaxHealth = rollHP();
+			Health = MaxHealth;
+        }
+
+        public override int MeleeAttack()
+        {
+            DieRoll Die = new DieRoll(1, 6);
+			return Die.Roll() + 3 + base.bonusPS();
+        }
+
+        public override int toHitMelee()
+        {
+            return base.toHitMelee() + 2;
+        }
+
+        public override int toParry()
+        {
+			return base.toParry() + 2;
+        }
+
+        public override int toDodge()
+        {
+			return base.toDodge() + 2;
+        }
+
+        public override int rollHP()
+        {
+            //Ensures the DieRoll will return a value between 35-50
+            return DieRoll.RollOne(6) + 5;
+        }
+
+        public override int rollsdc()
+        {
+            //Ensures the DieRoll will return a value between 60-80
+            return DieRoll.RollOne(16) + 32;
+        }
+
     }
 }
