d76d287fb3b64e368aad800f30e774403531bc1d
Christian Borntrager <christian.borntrager@gmail.com>
2014-08-26 15:46:35 -0600

    some quick changes

--- ZombieApocalypseSimulator/Models/Characters/Classes/Fighter.cs
+++ ZombieApocalypseSimulator/Models/Characters/Classes/Fighter.cs
@@ -3,11 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZombieApocalypseSimulator.Factories;
+using ZombieApocalypseSimulator.Models.Items;
 
 namespace ZombieApocalypseSimulator.Models.Characters.Classes
 {
     public class Fighter : Player
     {
+        public Fighter()
+        {
+            Name = "Phill";
+            Items = new List<Item>();
+            ItemLimit = 5;
+            //Items.Add(new MeleeWeapon { Condition = 100, Damage = "2d6", IsEquiped = false, 
+            //    MeleeWeaponType = MeleeWeaponType.Blunt, Name = "Small Crowbar", IgnoresAR = false });
+            Items.Add(new Health { AmountHealed = "2d10", Description = "A Medium Health-kit", Name = "Health Pack" });
+            Items.Add(WeaponFactory.GetInstance("Deagle|Ranged|Handgun|100|10d60|12"));
+            Items.Add(WeaponFactory.GetInstance("Small Crowbar|Melee|Blunt|100|2d6"));
+            //EquippedWeapon = (Weapon)Items.ElementAt(0);
+            IntelligenceQuotient = rollAttributes();
+            MentalEndurance = rollAttributes();
+            MentalAffinity = rollAttributes();
+            PhysicalStrength = rollAttributes();
+            PhysicalEndurance = rollAttributes();
+            PhysicalProwess = rollAttributes();
+            PhysicalBeauty = rollAttributes();
+            Speed = rollAttributes();
+            ArmorRating = 0;
+            base.SetLife();
+        }
+
         public int Damage()
         {
             int damage = 0;
@@ -21,6 +46,25 @@ namespace ZombieApocalypseSimulator.Models.Characters.Classes
                 damage = MeleeAttack().Damage;
             }
             return damage;
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            s += "Class: Fighter";
+            s += "\r\nHealth: " + Health + "/" + MaxHealth;
+            s += "\r\nSDC: " + sdc + "/" + MaxSDC;
+            s += "\r\nLevel: " + Level;
+            s += "\r\nSpeed: " + Speed;
+            s += "\r\nIQ: " + IntelligenceQuotient;
+            s += "\r\nME: " + MentalEndurance;
+            s += "\r\nMA: " + MentalAffinity;
+            s += "\r\nPS: " + PhysicalStrength;
+            s += "\r\nPP: " + PhysicalProwess;
+            s += "\r\nPB: " + PhysicalBeauty;
+            s += "\r\nPE: " + PhysicalEndurance;
+
+            return s;
         }
     }
 }
